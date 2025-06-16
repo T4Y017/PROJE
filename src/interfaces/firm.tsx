e533b982df70c015b235ef0c9727c5275da7787c
T4Y017 <167023111+T4Y017@users.noreply.github.com>
2025-06-16 11:35:39 +0300

    Firma görüntüleme düzenlendi
    * Bitmemiş hali ama devam ediyorum
    
    * Firma isimlerine tıklayınca modal açılıyor fakat backten veri çekme kısmına uğraşacağım
    
    * Backendten firma bilgileri çekildi çalışan bilgileri backendten çekilecek şuan static olarak aldım
    
    * Kullanıcı bilgileri backend den alınıp basıldı 1.issue bitti
    
    * Firma kısmını söylediğiniz hale getirdim
    
    * Çoğu düzeltmeyi yaptım getFirmInfo'nun istendiğini zaman çağırılmasını yapmaya çalıştım fakat bu sefer kapatma tuşuna basınca kapanmıyor ya da ben yanlış yaptım o kısmı yapamadım
    
    * Ts için birkaç değişiklik yaptım
    
    * Close tuşuna basınca kapanmama sorunu çözüldü getFirmInfo sonradan çalışıyor galiba ama emin değilim
    
    * Boşluklar silindi
    
    * Hemen firma bilgilerini alma düzeltildi

---
+++ src/interfaces/firm.tsx
@@ -0,0 +1,12 @@
+export default interface Firm {
+    id: number;
+    firmName: string;
+    firmMail: string;
+    address: string;
+    tel: string;
+    current_working_person: number;
+    firmType: string;
+    firmStatus: string;
+    latitude: number;
+    longitude: number;
+}
---
+++ src/interfaces/user.tsx
@@ -0,0 +1,14 @@
+export default interface User {
+    id: number;
+    username: string;
+    surname: string;
+    mail: string;
+    tel: string;
+    firmName: string;
+    firmId:number;
+    status:string;
+    role:string;
+    birthdate:string;
+    gender:string;
+    known_language:string;
+}
