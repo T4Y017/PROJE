ddc0ba20652317bf26e8a2195a5c08cc58243f30
T4Y017 <167023111+T4Y017@users.noreply.github.com>
2025-06-17 13:48:14 +0300

    Firma bilgilerini çeşitlendirme (#8)
    * Firma bilgileri çeşitlendirme
    
    * open user details
    
    * Spinner eklendi ve düzenlemeler yapıldı
    
    * Todo da tamamlandı
    
    * Firma bilgileri çeşitlendirme
    
    open user details
    
    Spinner eklendi ve düzenlemeler yapıldı
    
     Todo da tamamlandı
    
    ---------
    
    Co-authored-by: bilalkosker <bilal.kosker@sigun.com.tr>

--- src/interfaces/firm.tsx
+++ src/interfaces/firm.tsx
@@ -1,12 +1,12 @@
 export default interface Firm {
-    id: number;
-    firmName: string;
-    firmMail: string;
-    address: string;
-    tel: string;
+    id:number;
+    firmName:string;
+    firmMail:string;
+    address:string;
+    tel:string;
     current_working_person: number;
-    firmType: string;
-    firmStatus: string;
-    latitude: number;
-    longitude: number;
+    firmType:string;
+    firmStatus:string;
+    latitude:number;
+    longitude:number;
 }
