701e422854bb2459267f8655fab412415e2037d2
T4Y017 <oktay.ginis@ug.bilkent.edu.tr>
2025-06-17 16:35:02 +0300

    Navigation added

--- src/App.tsx
+++ src/App.tsx
@@ -1,46 +1,11 @@
-import {useEffect, useState} from 'react'
-import axios from 'axios'
-import './App.css'
-import {FirmInfoModal} from './components/firm-info-modal'
-import User from "./interfaces/user";
-import { UserInfoTable} from "./components/user-info-table";
-import {UserInfoModal} from "./components/user-info-modal";
+import {Outlet} from "react-router-dom";
 
 function App() {
-    const [isFirmModalOpen, setIsOpen] = useState<boolean>(false)
-    const [isUserModalOpen, setIsUserModalOpen] = useState<boolean>(false)
-
-    const handleCloseFirmModal = () => {
-        setIsOpen(false);
-    }
-    const handleCloseUserModal = () => {
-        setIsUserModalOpen(false);
-    }
-
-    const [firmID, setFirmID] = useState<number>();
-    const [userID, setUserID] = useState<number>();
-    const [employee, setUser] = useState<User[]>([]);
-
-    const getUserInfo = () => {
-        axios.get("http://localhost:3000/api/users").then((resp) => {
-                console.log(resp);
-               
-                setUser(resp.data.users);
-            }
-        );
-    };
-
-    useEffect(() => {
-        getUserInfo();
-    }, []);
-
     return (
-        <div className='App'>
-            <UserInfoTable setIsOpen={setIsOpen}  setFirmID={setFirmID} setUserID={setUserID} emp={employee} setIsUserModalOpen={setIsUserModalOpen}/>
-            {isFirmModalOpen && <FirmInfoModal onClose={handleCloseFirmModal} firmId={firmID}/>}
-            {isUserModalOpen && <UserInfoModal onClose={handleCloseUserModal} userId={userID}/>}
+        <div className="App">
+            <Outlet/>
         </div>
-    )
+    );
 }
 
-export default App
+export default App;
